875615165000ee0d5b0b4dced78d8ab9f371a452
adlermo <adlerorteney@gmail.com>
2022-06-29 22:37:22 -0300

    Added users controller

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -2,18 +2,55 @@ import { Request, Response } from 'express';
 import pool from '../common/postgres-connector';
 
 class ProductsController {
-  public async get(req: Request, res: Response) {
+  public async getAll(req: Request, res: Response) {
+    console.info(`PRODUCTS -> GET | Fetching products list`);
+
     try {
       const client = await pool.connect();
 
-      const sql = 'SELECT * FROM todos';
+      const sql = 'SELECT * FROM products';
       const { rows } = await client.query(sql);
-      const todos = rows;
+      const products = rows;
 
       client.release();
 
-      res.send(todos);
+      res.send(products);
+      console.info(`PRODUCTS -> GET | Succesfully fetched!`);
     } catch (error) {
+      console.error(`PRODUCTS -> GET | Failed fetching!`);
+      res.status(400).send(error);
+    }
+  }
+
+  public async create(req: Request, res: Response) {
+    const { user_id, name, description, price } = req.body;
+
+    console.info(
+      `PRODUCTS -> POST | Creating product ${JSON.stringify(
+        req.body
+      )} to user ${user_id}`
+    );
+
+    try {
+      const client = await pool.connect();
+
+      const sql = `INSERT INTO products(id_product, user_id, name, description, price, published_at) VALUES(nextval('products_sequence').$1,$2,$3,$4,$5);`;
+      const values = [user_id, name, description, price, new Date()];
+
+      let result = await client.query(sql, values);
+
+      client.release();
+
+      if (result.command != 'INSERT')
+        throw Error(
+          'Could not save product to database, please try again latter!'
+        );
+
+      res.status(201).send();
+
+      console.info(`PRODUCTS -> POST | Succesfully created!`);
+    } catch (error) {
+      console.error(`PRODUCTS -> POST | Failed creating!`);
       res.status(400).send(error);
     }
   }
---
+++ src/controllers/users-controller.ts
@@ -0,0 +1,55 @@
+import { Request, Response } from 'express';
+import pool from '../common/postgres-connector';
+
+class UsersController {
+  public async getAll(req: Request, res: Response) {
+    console.info(`USERS -> GET | Fetching users list`);
+
+    try {
+      const client = await pool.connect();
+
+      const sql = 'SELECT * FROM users';
+      const { rows } = await client.query(sql);
+
+      const users = rows;
+
+      client.release();
+
+      res.send(users);
+
+      console.info(`USERS -> GET | Succesfully fetched!`);
+    } catch (error) {
+      console.error(`USERS -> GET | Failed fetching!`);
+      res.status(400).send(error);
+    }
+  }
+
+  public async create(req: Request, res: Response) {
+    const { name, email, password } = req.body;
+
+    console.info(`USERS -> POST | Creating user ${JSON.stringify(req.body)}`);
+
+    try {
+      const client = await pool.connect();
+
+      const sql = `INSERT INTO users(id_user, name, email, passw) VALUES(nextval('users_sequence'),$1,$2,$3);`;
+      const values = [name, email, password];
+
+      let result = await client.query(sql, values);
+
+      client.release();
+
+      if (result.command != 'INSERT')
+        throw Error("Couldn't save user to database, please try again latter!");
+
+      res.status(201).send();
+
+      console.info(`USERS -> POST | Succesfully created!`);
+    } catch (error) {
+      console.error(`USERS -> POST | Failed creating!`);
+      res.status(400).send(error);
+    }
+  }
+}
+
+export default UsersController;
